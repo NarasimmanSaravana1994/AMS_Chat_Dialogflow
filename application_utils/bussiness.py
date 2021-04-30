39b6bcafcd6c91a14cfb6b0aa0dd01a27bcd33d3
Narasimman <41271370+NarasimmanSaravana1994@users.noreply.github.com>
2021-05-01 00:54:25 +0530

    sample dialogflow json folder added and minor code changes.

--- application_utils/bussiness.py
+++ application_utils/bussiness.py
@@ -11,11 +11,25 @@ def verify_user_answer(company_id: int, domain_id: int, question_id: int, answer
     for index, base_flow in enumerate(base_flows):
         if base_flow["question_id"] == question_id:
             if base_flow["is_db_provider"] and len(base_flow["answers"]):
-                return True, base_flow["next_question_id"][0]
+                return True, base_flow["next_question_id"]
             else:
                 for base_flow_answer in base_flow["answers"]:
                     if answer in base_flow_answer:
                         return True, base_flow_answer["mapped_question_id"]
+
+
+def identify_next_dialogflow(company_id: int, domain_id: int, question_id: int) -> responce_model_object:
+
+    base_flows = mongo_db_util.get_base_flow(
+        company_id=company_id, domain_id=domain_id)
+
+    chat_responce = []
+    for base_flow in enumerate(base_flows):
+
+        if base_flow['question_id'] == question_id:
+            chat_responce = base_flow
+            break
+    return chat_responce
 
 
 def chat_dialogflow(session_id: int, company_id: int, domain_id: int) -> responce_model_object:
@@ -30,6 +44,13 @@ def chat_dialogflow(session_id: int, company_id: int, domain_id: int) -> responc
 
     ## to do part ##
     next_dialogflow = {}
+
+    if is_user_answer_verified:
+        next_dialogflow = identify_next_dialogflow(
+            company_id=company_id, domain_id=domain_id, question_id=next_question_id)
+    else:
+        next_dialogflow = identify_next_dialogflow(
+            company_id=company_id, domain_id=domain_id, question_id=question_id)
 
     if next_dialogflow["flow_end"]:
         email.Email_triggering(session_id, company_id,
